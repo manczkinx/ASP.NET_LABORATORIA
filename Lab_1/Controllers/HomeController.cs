df01e5f4442fa91c186341b2e55168958267244b
manczkinx <sepioool24@gmail.com>
2023-11-23 23:21:12 +0100

    14

--- Lab_1/Controllers/HomeController.cs
+++ Lab_1/Controllers/HomeController.cs
@@ -2,31 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace Lab_1.Controllers
+namespace Lab_1.Controllers;
+
+public enum Operators
 {
-    public class HomeController : Controller
+    add, mul, sub, div
+}
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
